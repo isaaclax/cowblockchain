2e5ca5dbbaa8f78ed22fc4c23ab045363f966d51
Isaac Lax <Isaac.Lax@us.qbe.com>
2016-11-03 13:52:53 -0500

    Updating chaincode.go

--- chaincode.go
+++ chaincode.go
@@ -1,10 +1,10 @@
 package main
 
 import(
-	"encoding/json"
+// "encoding/json"
 	"errors"
 	"fmt"
-	"strconv"
+// "strconv"
 //	"time"
 //	"string"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
