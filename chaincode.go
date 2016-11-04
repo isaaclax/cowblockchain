f109f3099cb1bf4395c4f5d6d1798bf405cc97e5
Isaac Lax <Isaac.Lax@us.qbe.com>
2016-11-04 07:39:45 -0500

    updating chaincode.go

--- chaincode.go
+++ chaincode.go
@@ -5,36 +5,49 @@ import(
 	"errors"
 	"fmt"
 // "strconv"
-//	"time"
+	"time"
 //	"string"
+	"github.com/satori/go.uuid"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 type SimpleChaincode struct {}
 
 type Cow struct {
-	ID string
-	OwnerID string
-	SensorID string
+	ID string 'json:"id"'
+	OwnerID string 'json:"ownerID"'
+	SensorID string 'json:"sensorID"'
 }
 
 type Owner struct {
-	CowsOwned []Cow
-	Policies []Policy
+	CowsOwned []Cow 'json:"cow"'
+	Policies []Policy 'json:"policies"'
 }
 
 type Sensor struct {
-	ID string
-	CowID string
+	ID string 'json:"id"'
+	CowID string 'json:"cowID"'
 }
 
 type Policy struct {
-	ID string
-	CowID []Cow
-	OwnerID []Owner
-	Premium int32
-	Value int32
+	ID string 'json:"id"'
+	CowID []Cow 'json:"cowID"'
+	OwnerID []Owner 'json:"ownerID"'
+	Premium int32 'json:"premium"'
+	Value int32 'json:"value"'
 }
+
+// =======================================================================================================================
+// Make Timestamp - create a timestamp in ms
+// =======================================================================================================================
+
+func makeTimestamp() int64 {
+    return time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond))
+}
+
+//========================================================================================================================
+// Main
+//========================================================================================================================
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
@@ -43,7 +56,10 @@ func main() {
 	}
 }
 
+//========================================================================================================================
 // Initialize the state of the 'Policies' variable
+//========================================================================================================================
+
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting 1")
@@ -57,21 +73,97 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
     return nil, nil
 }
 
+//========================================================================================================================
+// Initialize the state of the 'Policies' variable
+//========================================================================================================================
+
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
     fmt.Println("invoke is running " + function)
 
     // Handle different functions
+
+		//initialize the chaincode state, used as reset
     if function == "init" {
         return t.Init(stub, "init", args)
-    } else if function == "write" {
+    }
+		//deletes an entity from its state
+		else if function == "delete" {
+			return t.Delete(stub, args)
+		}
+		//writes a value to the chaincode state
+		else if function == "write" {
         return t.write(stub, args)
     }
+		//create a new cow
+		else if function == "init_cow" {
+			return t.init_cow(stub, args)
+		}
+		//change owner of a cow
+		else if function == "set_user" {
+			return t.set_user(stub, args)
+		}
     fmt.Println("invoke did not find func: " + function)
 
     return nil, errors.New("Received unknown function invocation")
 }
 
+//========================================================================================================================
+// Registers a cow to the blockchain
+//========================================================================================================================
+
+func (t *SimpleChaincode) registerCow(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+		if len(args) < 2 {
+			return nil, errors.New("Incorrect number of arguments. Expecting 2. ID of the owner and the ID of the sensor")
+		}
+
+		cowID := uuid.NewV4().String()
+		ownerID := args[0]
+		sensorID := args[1]
+
+		var newCow Cow
+		newCow.ID = cowID
+		newCow.OwnerID = ownerID
+		newCow.SensorID = sensorID
+
+		if err != nil {
+        return nil, err
+    }
+    return nil, nil
+}
+
+//========================================================================================================================
+// Registers a policy to the blockchain
+//========================================================================================================================
+
+func (t *SimpleChaincode) registerPolicy(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	if len(args) < 4 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 4. ID of the cow, ID of the owner, the premium, and the value of the policy")
+	}
+
+	policyID := uuid.NewV4().String()
+	cowID := args[0]
+	ownerID := args[1]
+	premium := args[2]
+	value := args[3]
+
+	var newPolicy Policy
+	newPolicy.ID = policyID
+	newPolicy.CowID = cowID
+	newPolicy.OwnerID = ownerID
+	newPolicy.Premium = premium
+	newPolicy.Value = value
+
+	if err != nil {
+			return nil, err
+	}
+	return nil, nil
+
+}
+
+//========================================================================================================================
 // Check the state of the chaincode
+//========================================================================================================================
+
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
     fmt.Println("query is running " + function)
 
@@ -84,7 +176,10 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
     return nil, errors.New("Received unknown function query")
 }
 
+//========================================================================================================================
 // Write a value to a variable
+//========================================================================================================================
+
 func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
     var name, value string
     var err error
@@ -103,7 +198,10 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
     return nil, nil
 }
 
+//========================================================================================================================
 // Read the state of a variable
+//========================================================================================================================
+
 func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
     var name, jsonResp string
     var err error
