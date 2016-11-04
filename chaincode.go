f4915379c89f4ccd77e6a84619c78857b02db4bb
Isaac Lax <Isaac.Lax@us.qbe.com>
2016-11-04 09:49:52 -0500

    updating chaincode.go

--- chaincode.go
+++ chaincode.go
@@ -110,7 +110,9 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 // Registers a policy to the blockchain
 //========================================================================================================================
 
-func (t *SimpleChaincode) registerPolicy(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+func (t *SimpleChaincode) init_policy(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var err error
+
 	if len(args) < 4 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 4. ID of the cow, ID of the owner, the premium, and the value of the policy")
 	}
@@ -127,12 +129,28 @@ func (t *SimpleChaincode) registerPolicy(stub *shim.ChaincodeStub, args []string
 		return nil, err
 	}
 
-	var newPolicy Policy
-	newPolicy.ID = policyID
-	newPolicy.CowID = cowID
-	newPolicy.OwnerID = ownerID
-	newPolicy.Premium = premium
-	newPolicy.Value = value
+	str := "{policyID: " + policyID + "cowID: " + cowID + ", ownerID: " + ownerID + ", premium: " + premium + ", value: " + value + "}"
+	err = stub.PutState(policyID, []byte(str))								//store policy with id as key
+	if err != nil {
+		return nil, err
+	}
+
+	//get the policy index
+	policyAsBytes, err := stub.GetState(policyIndexStr)
+	if err != nil {
+		return nil, errors.New("Failed to get policy index")
+	}
+	var policyIndex []string
+	json.Unmarshal(policyAsBytes, &policyIndex)							//un stringify it aka JSON.parse()
+
+	//append
+	policyIndex = append(policyIndex, args[0])								//add policy name to index list
+	fmt.Println("! policy index: ", policyIndex)
+	jsonAsBytes, _ := json.Marshal(policyIndex)
+	err = stub.PutState(policyIndexStr, jsonAsBytes)						//store name of cow
+
+	fmt.Println("- end init policy")
+	return nil, nil
 
 }
 
@@ -167,8 +185,8 @@ func (t *SimpleChaincode) init_cow(stub *shim.ChaincodeStub, args []string) ([]b
 		return nil, err
 	}
 
-	//get the marble index
-	marblesAsBytes, err := stub.GetState(cowIndexStr)
+	//get the cow index
+	cowsAsBytes, err := stub.GetState(cowIndexStr)
 	if err != nil {
 		return nil, errors.New("Failed to get cow index")
 	}
