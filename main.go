bbf5ab53db66ccacb88b496877d2ff5a053212b5
Isaac Lax <Isaac.Lax@us.qbe.com>
2016-11-15 11:10:39 -0600

    updating main.go

--- main.go
+++ main.go
@@ -1,37 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"errors"
+	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"strconv"
 )
 
+type SimpleChaincode struct {
+}
+
 type Owner struct {
-	OwnerID string 'json:"ownerID"'
-	CowsOwned []Cows 'json:"cows"'
+	OwnerID string `json:"ownerID"`
+	CowsOwned []Cow `json:"cows"`
 	//TODO(isaac) should never have more policies than cows
 }
 
 type Cow struct {
-	OwnerID string 'json:"ownerID"'
-	SensorID string 'json:"sensorID"'
+	OwnerID string `json:"ownerID"`
+	SensorID string `json:"sensorID"`
 }
 
 type Policy struct {
-	PolicyID string 'json:"policyID"'
-	Cow []Cow 'json:"cow"'
-	Premium int 'json:"premium"'
-	Value int 'json:"value"'
+	PolicyID string `json:"policyID"`
+	Cow []Cow `json:"cow"`
+	Premium int `json:"premium"`
+	Value int `json:"value"`
 }
 
 type AllCows struct {
-	Catalog []Cow 'json:"cows"'
+	Catalog []Cow `json:"cows"`
 }
 
 type AllOwners struct {
-	Catalog []Owner 'json:"owners"'
+	Catalog []Owner `json:"owners"`
 }
 
 type AllPolicies struct {
-	Catalog []Policy 'json:"policies"'
+	Catalog []Policy `json:"policies"`
 }
 
 var activePoliciesString = "_activePolicies"
@@ -81,6 +88,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	//Create and marshal the active policies
 	var activePolicies AllPolicies
 	activePolicies.Catalog = policyCatalog
+	var policiesAsBytes []byte
 	policiesAsBytes, err := json.Marshal(activePolicies)
 	if err != nil {
 		return nil, err
@@ -89,7 +97,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	// Create and marshal the pending policies
 	var activeCows AllCows
 	activeCows.Catalog = cowCatalog
-	var pendingAsBytes []byte
+	var cowsAsBytes []byte
 	cowsAsBytes, err = json.Marshal(activeCows)
 	if err != nil {
 		return nil, err
@@ -98,13 +106,13 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	// Create and marshal incomplete policies
 	var activeOwners AllOwners
 	activeOwners.Catalog = ownerCatalog
-	var incompleteAsBytes []byte
+	var ownersAsBytes []byte
 	ownersAsBytes, err = json.Marshal(activeOwners)
 	if err != nil {
 		return nil, err
 	}
 
-	err = stub.PutState(activePoliciesString, activeAsBytes)
+	err = stub.PutState(activePoliciesString, policiesAsBytes)
 	if err != nil {
 		fmt.Println("Failed to initialize policies")
 		return nil, err
@@ -119,7 +127,10 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	err = stub.PutState(activeOwnersString, ownersAsBytes)
 	if err != nil {
 		fmt.Println("Failed to initialize active owners")
-		return nil, err
+		return nil, err}
+
+	fmt.Println("Initializatin Complete")
+	return nil, nil
 }
 
 //==============================================================================
@@ -208,28 +219,28 @@ func (t *SimpleChaincode) registerOwner(stub *shim.ChaincodeStub, args []string)
 	//TODO(isaac) assign the owner a unique ID #
 	//TODO(isaac) make an owner object with this ID #
 	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2 (First & Last name)"
+		return nil, errors.New("Incorrect number of arguments. Expecting 2 (First & Last name)")
 	}
 
 	var owner Owner
 	owner.OwnerID = makeHash(args)
-
+	return nil, nil
 }
 
 //==============================================================================
 //==============================================================================
 
-func (t *SimpleChaincode) registerCow(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+func (t *SimpleChaincode) registerCow(stub *shim.ChaincodeStub, ownerID string, sensorID string) ([]byte, error) {
 
-	if len(args) != 2 {
+	if ownerID == "" || sensorID == "" {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2 (OwnerID & SensorID)")
 	}
 
 	//TODO(isaac) assign the cow a unique ID #
 	//sensor ID should be argument, can be unique ID #
 
-	ownerID = args[0]					// owner ID # should be passed as an argument
-	cowID = args[1]				// the sensor ID # should be passed as an argument and will be assigned to the cow as it's unique ID #
+	//ownerID := args[0]					// owner ID # should be passed as an argument
+	//sensorID := args[1]				// the sensor ID # should be passed as an argument and will be assigned to the cow as it's unique ID #
 
 	//TODO(isaac) check to see if the SensorID has already been registered
 	// if it has, reject the attempt
@@ -237,11 +248,11 @@ func (t *SimpleChaincode) registerCow(stub *shim.ChaincodeStub, args []string) (
 	//TODO(isaac) use the CowID, OwnerID, and SensorID to make a cow object
 	// convert the cow object to JSON?
 	var cow Cow
-	cow.ownerID = ownerID
-	cow.CowID = cowID
+	cow.OwnerID = ownerID
+	cow.SensorID = sensorID
 
 	//TODO(isaac) pull the entire list of cows from the blockchain
-	cowsAsBytes, err := getCows(stub, allCowsString)
+	cowsAsBytes, err := getCows(stub, activeCowsString)
 	if err != nil {
 		return nil, err
 	}
@@ -251,10 +262,11 @@ func (t *SimpleChaincode) registerCow(stub *shim.ChaincodeStub, args []string) (
 
 	//TODO(isaac) add the list of cows to the blockchain
 
-	err = stub.PutState(cowID, []byte(str))					//store cow with id as key
+	err = stub.PutState(cow.SensorID, cowsAsBytes)					//store cow with id as key
 	if err != nil {
 		return nil, err
 	}
+	return nil, nil
 }
 
 //==============================================================================
@@ -263,7 +275,7 @@ func (t *SimpleChaincode) registerCow(stub *shim.ChaincodeStub, args []string) (
 func addOwner(stub *shim.ChaincodeStub, owner Owner) error {
 	fmt.Println("Function: addOwner")
 
-	ownersAsBytes, err := getOwners(stub, OwnerString)
+	ownersAsBytes, err := getOwners(stub, activeOwnersString)
 	if err != nil {
 		return err
 	}
@@ -280,7 +292,7 @@ func addOwner(stub *shim.ChaincodeStub, owner Owner) error {
 	fmt.Println("owner appended to list of owners")
 
 	ownersAsBytes, err = json.Marshal(listOfOwners)
-	err = write(stub, OwnerString, ownersAsBytes)
+	err = write(stub, activeOwnersString, ownersAsBytes)
 	if err != nil {
 		return err
 	}
@@ -294,7 +306,7 @@ func addOwner(stub *shim.ChaincodeStub, owner Owner) error {
 func addCow(stub *shim.ChaincodeStub, cow Cow) error {
 	fmt.Println("Function: addCow")
 
-	cowsAsBytes, err := getCows(stub, CowString)
+	cowsAsBytes, err := getCows(stub, activeCowsString)
 	if err != nil {
 		return err
 	}
@@ -311,7 +323,7 @@ func addCow(stub *shim.ChaincodeStub, cow Cow) error {
 	fmt.Println("cow appended to list of cows")
 
 	cowsAsBytes, err = json.Marshal(listOfCows)
-	err = write(stub, CowString, cowsAsBytes)
+	err = write(stub, activeCowsString, cowsAsBytes)
 	if err != nil {
 		return err
 	}
@@ -352,15 +364,15 @@ func bytesToAllCows(cowsAsBytes []byte) (AllCows, error) {
 //==============================================================================
 //==============================================================================
 
-func createPolicyObject(args []string) Policy {
+func createPolicyObject(ID string, cows []Cow) Policy {
 	fmt.Println("Function: createPolicyObject")
 
 	//TODO(isaac) I need to enter the cow object, the premium, and the value to the policy object
 
 	var policy Policy
 
-	policy.PolicyID = makeHash(args)
-	policy.Cow = args[0]
+	policy.PolicyID = ID
+	policy.Cow = cows
 	policy.Premium = 100
 	policy.Value = 5000
 
@@ -386,7 +398,7 @@ func generatePolicy(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 
 	//TODO(isaac) add the new policy to the list of current policies
 	// add the policy to the owner
-	policiesAsBytes, err := getPolicies(stub, incompletePoliciesString)
+	policiesAsBytes, err := getPolicies(stub, activePoliciesString)
 	if err != nil {
 		return nil, err
 	}
@@ -437,4 +449,15 @@ func payOut(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	//TODO(isaac) delete the policy from the list of current policies
 
 	//
+	return nil
+}
+
+func write(stub *shim.ChaincodeStub, name string, value []byte) error {
+	fmt.Println("Function: write")
+
+	err := stub.PutState(name, value)
+	if err != nil {
+		return err
+	}
+	return nil
 }
